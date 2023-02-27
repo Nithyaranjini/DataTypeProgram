154ad410a8d37628a63412a9e83765bd39203253
Nithyaranjini <srinithya2k@gmail.com>
2023-02-27 17:52:22 +0530

    commit datatype program

---
+++ DataType Sample Program-ProjectDetails/src/org/tcs/test/login/CourseDetails.java
@@ -0,0 +1,59 @@
+package org.tcs.test.login;
+
+public class CourseDetails {
+	public static void main(String[] args) {
+		byte age =30;
+		System.out.println("my age is :"+age);
+	    Short atmPin = 2345;
+	    System.out.println("my atmPin is:"+atmPin);
+	    int Pincode = 234567;
+	    System.out.println("my pincode is :"+Pincode);
+	    long Phno = 2345678910l;
+	    System.out.println("my Phno is:"+Phno);
+	    float Salary=2345.456678f;
+	    System.out.println("my salary is :"+Salary);
+	    char gender = 'F';
+	    System.out.println("my gender is: "+gender);
+	    char a = ' ';
+	    System.out.println(a);
+	    String name= "Raj";
+	    System.out.println("my name is :"+name);
+	    String dob="15/11/2000";
+	    System.out.println("my dob is :"+dob);
+	    
+	    String email= "srinithya332k@gmail.com";
+	    System.out.println("my email is :"+email);
+	    boolean a1=true;
+	    System.out.println(a1);
+	    boolean a2=false;
+	    System.out.println(a2);}}
+	    
+	   
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+		
+
+		
+	    
+
+		
+
+		
+
+
+
+		
+
+	
+
+	
+		
+	
+
+
